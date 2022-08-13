c7291441267f61bf31d0b7556d82f9511d79875a
Kyle Benson <kyleabenson@gmail.com>
2022-08-13 16:09:04 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+
+
+app = FastAPI()
+
+FastAPIInstrumentor.instrument_app(app)
+
+class Item(BaseModel):
+    id: int
+    description: str
+    dueDate: Union[str, None] = None
+
+
+todos = {}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/todo")
+def get_items() -> dict:
+    return { "data" : todos}
+
+@app.post("/todo")
+async def add_item(item: Item):
+    todos[item.id] = item
+    return todos
+
+
