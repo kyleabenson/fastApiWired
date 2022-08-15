4813448db78a519ca40916a408d750e26965a5d5
Kyle Benson <kyleabenson@gmail.com>
2022-08-15 15:13:47 +0000

    Added Logging and Prometheus support

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import uvicorn
+import logging
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheusrock import PrometheusMiddleware, metrics_route
 #from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 
 
-app = FastAPI()
 
+
+app = FastAPI()
+#Add Prometheus Support
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics_route)
+
+#Otel Instrumentor
 #FastAPIInstrumentor.instrument_app(app)
+
+# Add Logging Config 
+log = logging.getLogger(__name__)
+FORMAT = "%(levelname)s:%(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 class Item(BaseModel):
     id: int
@@ -24,11 +37,13 @@ def read_root():
 
 @app.get("/todo")
 def get_items() -> dict:
+    log.debug("Request for todos")
     return { "data" : todos}
 
 @app.post("/todo")
 async def add_item(item: Item):
     todos[item.id] = item
+    log.debug("Add item to todo list")
     return todos
 
 
